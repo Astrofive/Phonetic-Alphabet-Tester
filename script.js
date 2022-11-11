6a43b2ca94876bcc6bead906867b2ebbf6a3d443
Kevin <kevin.strebel@yahoo.com>
2022-11-11 12:55:17 -0600

    lil easter eggs

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
+let eastereggcount = 0
+
 //program button to refresh text when clicked
 const refreshbutton = document.querySelector('#refresh');
 refreshbutton.addEventListener('click', () => {
     refresh(10);
     refreshbutton.style.backgroundColor = "";
+    eastereggcount += 1;
 });
 
 // refresh() generates 10 random letters and updates document
@@ -13,6 +16,14 @@ function refresh(length){
     for (let i = 0; i < length; i++){
         result += letters.charAt(Math.floor(Math.random() * letters.length));
     } 
+    if (eastereggcount == 10) {
+        console.log(eastereggcount);
+        result = "HOWDYBOYS!"
+    }
+    if (eastereggcount == 100) {
+        console.log(eastereggcount);
+        result = "DAMN,100??"
+    }
     console.log(result);
     document.querySelector('#textbox').textContent = result;
 }
